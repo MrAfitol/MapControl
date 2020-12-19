acdbaea561224b9de838b4102383e34c66b06c4a
TheVoidNebula <75329526+TheVoidNebula@users.noreply.github.com>
2020-12-19 16:49:27 +0100

    yes

--- MapControl/Plugin.cs
+++ MapControl/Plugin.cs
@@ -13,10 +13,10 @@ namespace MapControl
 {
     public class Plugin : Plugin<Config>
     {
-        public override string Author { get; } = "Developed by TheVoidNebula";
+        public override string Author { get; } = "TheVoidNebula";
         public override string Name { get; } = "MapControl";
         public override string Prefix { get; } = "MapControl";
-        public override Version Version { get; } = new Version(1, 1, 0);
+        public override Version Version { get; } = new Version(1, 2, 0);
 
         public static Plugin Singleton;
 
